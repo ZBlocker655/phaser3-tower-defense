33263f825ca5f946442d70b8615d860cb06416cf
Zach Blocker <zach.blocker@gmail.com>
2023-02-11 15:25:14 -0500

    Lesson 2 - refactor

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import config from './config/config';
+import MainScene from './scenes/MainScene';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add('Main', MainScene);
+    this.scene.start('Main');
+  }
 }
 
-const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
-};
-
-const game = new Phaser.Game(config);
+window.onload = function() {
+  window.game = new Game();
+}
